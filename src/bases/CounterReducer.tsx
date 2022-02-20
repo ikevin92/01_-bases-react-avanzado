31b654001dfcf950718cfa9a69a156d9751020f8
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-02-20 00:27:37 -0500

    add reducer class

--- src/bases/CounterReducer.tsx
+++ src/bases/CounterReducer.tsx
@@ -1,17 +1,67 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+interface CounterState {
+  counter: number;
+  previous: number;
+  changes: number;
+}
+
+// estado inicial del reducer
+const INITIAL_STATE: CounterState = {
+  counter: 10,
+  previous: 0,
+  changes: 0,
+};
+
+type CounterAction =
+  | { type: 'increaseBy'; payload: { value: number } }
+  | { type: 'reset' };
+
+export const counterReducer = (
+  state: CounterState,
+  action: CounterAction,
+): CounterState => {
+  switch (action.type) {
+    case 'reset':
+      return {
+        counter: 0,
+        previous: 0,
+        changes: 0,
+      };
+
+    case 'increaseBy':
+      return {
+        ...state,
+        changes: state.changes + 1,
+        previous: state.counter,
+        counter: action.payload.value + state.counter,
+      };
+
+    default:
+      return state;
+  }
+};
 
 const CounterReducerComponent = () => {
-  const [counter, setCounter] = useState<number>(0);
+  const [state, dispatch] = useReducer(counterReducer, INITIAL_STATE);
 
-  const handleClick = () => {
-    setCounter((prev) => prev + 1);
+  const handleReset = () => {
+    dispatch({ type: 'reset' });
+  };
+
+  const increaseBy = (value: number) => {
+    dispatch({ type: 'increaseBy', payload: { value } });
   };
 
   return (
     <>
-      <h1>Counter Reducer: {counter}</h1>
+      <h1>Counter Reducer: {state.counter}</h1>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
 
-      <button onClick={handleClick}>+1</button>
+      <button onClick={() => increaseBy(1)}>+1</button>
+      <button onClick={() => increaseBy(5)}>+5</button>
+      <button onClick={() => increaseBy(10)}>+10</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 };
